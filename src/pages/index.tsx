ad1efeccf992950a54a73cbe9c8352a9a1fcba82
Joseph De La Cruz <razorij20@gmail.com>
2023-04-01 22:47:44 -0500

    favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import { RecentView } from '@/views/RecentView'
 import { MainTemplate } from '@/templates/MainTemplate/MainTemplate'
+import Head from 'next/head'
 
 export default function Home () {
   return (
-    <MainTemplate>
-      <RecentView />
-    </MainTemplate>
+    <>
+      <Head>
+        <title>Music Player</title>
+        <meta name='description' content='Reproductor de musica ' />
+      </Head>
+      <MainTemplate>
+        <RecentView />
+      </MainTemplate>
+    </>
   )
 }
